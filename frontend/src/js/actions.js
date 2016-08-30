34eb392cb2cc07364bc63a30517b57f00adcf263
Eric Snell <eric_snell21@yahoo.com>
2016-08-30 15:55:50 -0400

    Updated Fetch Actions

--- frontend/src/js/actions.js
+++ frontend/src/js/actions.js
@@ -1,19 +1,62 @@
-import fetch from 'isomorphic-fetch';
+/*------------ FETCH ACTIONS -------------*/
+import fetch from 'isomorphic-fetch'
 
-//  Creating constants
+/* {
+french: 'de'  
+english: 'of, from, by, than, in, with'
+weight: 10
+}
+
+The GET enpoint to get the next word pair is ‘/word’ and the PUT enpoint for updating the words ‘’weight” or memory value is ‘/submitanswer'
+*/
+
 const GET_PAIR = 'GET_PAIR'
-const UPDATE_WORD = 'UPDATE_WORD'
+let getPair = (words) => {
+  return {
+    type: GET_PAIR,
+    words: words
+  }
+}
 
-//  creating/exporting action creators
-export let getWords = () => {
-	const url = 'http://localhost:3000/words'
+const UPDATE_WORD = 'UPDATE_WORD'
+let updateWord = (word) => {
+  return {
+    type: UPDATE_WORD,
+    word: word
+  }
+}
+
+// GET Request for Word Pair
+export let getWords = (username) => {
+	const url = 'http://localhost:3000/word'
   return fetch (url, {
   	method: 'GET'
-  }).then(function (words) {
-  	words: words 
-    type: GET_PAIR,
-  };
-};
+  })
+  .then((response) => {
+    console.log(response, ' <--response')
+    if (response.status < 200 || response.status >= 300) {
+      let error = new Error(response.statusText)
+      error.response = response
+      throw error
+    }
+    return response
+  })
+  .then((response) => {
+    return resonse.json()
+  })
+  .then((data) => {
+    console.log(data, ' <--data')
+  })
+
+}
+
+
+
+
+
+
+
+
 
 // Other functions
 let fetchWords = () => {
@@ -21,25 +64,8 @@ let fetchWords = () => {
 	// return value
 }
 
-let updateWord = (updatedWord) => {
-	getWords().then(function (resp) {
-		words = resp;
-	});
-  return {
-    type: UPDATE_WORD,
-    word: updatedWord
-  };
-};
-
-// exports.getWords = getWords;
 
 
-/* {
-french: 'de'	
-english: 'of, from, by, than, in, with'
-weight: 10
 
-}
+/*----------- ACTIONS ------------*/
 
-The GET enpoint to get the next word pair is ‘/words’ and the PUT enpoint for updating the words ‘’weight” or memory value is ‘/submitanswer'
-*/
