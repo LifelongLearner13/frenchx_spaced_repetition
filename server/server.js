cc199fe685f1e56b3bf2bd09e9550b8a7d0bf536
Sierra Gregg <sierragregg@Gmail.com>
2016-08-31 12:24:01 -0500

    added logout

--- server/server.js
+++ server/server.js
@@ -93,6 +93,12 @@ app.get('/auth/google/callback',
                 failureRedirect : '/'
         }));
 
+// LOGOUT ==============================
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
