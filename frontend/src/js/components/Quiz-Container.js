39528c5051449e9cddf4132f8c0107b7b17f381f
Eric Snell <eric_snell21@yahoo.com>
2016-09-01 16:38:42 -0400

    Code more DRY, combined fetch actions into one, removed unnecessary actions

--- frontend/src/js/components/Quiz-Container.js
+++ frontend/src/js/components/Quiz-Container.js
@@ -11,35 +11,33 @@ var Score = require('./score');
 var QuizContainer = React.createClass({
 
   checkAnswer: function(answer) {
-    console.log('in checkAnswer, answer is ', answer);
     var word2Array = this.props.word2;
     var foundWord;
-    console.log(this.props.word2, '<-- word2 Props')
-    console.log(this.props.wordId, '<-- Word ID Props')
 
     // Loops through English words array for user's answer.  
     for (var i = 0; i < word2Array.length; i++) {
       // If a match, show 'Correct!', increase score, fetch new words, submit to backend
       if (answer.toLowerCase() === word2Array[i].toLowerCase()) {
         foundWord = true;
-            this.props.dispatch(actions.correctDisplay());
-            this.props.dispatch(actions.incrementScore());
-            //this.props.dispatch(actions.fetchWords());
-            this.props.dispatch(actions.fetchSubmit(this.props.wordId, 'true'))
-            break
+        let score = this.props.score + 10
+        this.props.dispatch(actions.correctDisplay());
+      //  this.props.dispatch(actions.incrementScore());
+        this.props.dispatch(actions.fetchSubmit('', '', score))
+        break
       } 
       // If not match, decrement score and fetch new word pair
       if (i === word2Array.length - 1 && !foundWord) {
-          this.props.dispatch(actions.decrementScore());
-          //this.props.dispatch(actions.fetchWords());
-          this.props.dispatch(actions.fetchSubmit(this.props.wordId, 'false'))
+        let score = this.props.score - 10
+      //  this.props.dispatch(actions.decrementScore());
+        this.props.dispatch(actions.fetchSubmit(this.props.wordId, 'false', score))
       }
     }
   },  
 
   // Fetches new word pair on initial mounting of component
   componentDidMount: function() {
-    this.props.dispatch(actions.fetchSubmit("",""))
+    // On initial mount, send empty strings and receive a random word pair back
+    this.props.dispatch(actions.fetchSubmit('', '', ''))
   },
 
   logOutUser: function() {
@@ -52,6 +50,7 @@ var QuizContainer = React.createClass({
   },
 
   render: function() {
+
     return (
       <div className="Quiz">
         <div className="Quiz-header">
@@ -63,18 +62,17 @@ var QuizContainer = React.createClass({
         <Answer checkAnswer={this.checkAnswer} word2={this.props.word2} answerInput={this.props.answerInput} />
         <Score score={this.props.score} />
       </div>
-
     );
   }
 });
 
 var mapStateToProps = function(state, props) {
   return {
-    correct: state.correct,
     word1: state.word1,
     word2: state.word2,
     wordId: state.wordId,
     score: state.score,
+    correct: state.correct,
     answerInput: state.answerInput
   }
 };
